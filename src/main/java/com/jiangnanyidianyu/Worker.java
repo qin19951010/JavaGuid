058f8acdef4fabf1cd4a257fc0e6a04bab0bcbfa
秦忠良 <qinzhongliangzhuojianchina.com>
2021-08-03 17:38:08 +0800

    2021-08-03 17:37:25 入库

---
+++ src/main/java/com/jiangnanyidianyu/Worker.java
@@ -0,0 +1,19 @@
+package com.jiangnanyidianyu;
+
+import com.jiangnanyidianyu.BaseDemo.User;
+
+/**
+ * @Description TODO
+ * @Author Qinnn丶
+ * @Date 2021/6/24 0:04
+ **/
+public class Worker extends User {
+//    public static void ShowMsg()
+//    {
+//        System.out.println("this is a showMsg!");
+//    }
+
+    public static void main(String[] args) {
+        ShowMsg();
+    }
+}
